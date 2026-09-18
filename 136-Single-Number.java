05e63e9ff867d25e2291eed634356502f17ff96b
Arpit Sharma <arpitvgu.ac.in@gmail.com>
2026-09-18 23:26:08 +0530

    [136] [Time Beats: 99.94%] [Memory Beats: 45.95%] - LeetPush

---
+++ 136-Single-Number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+          int result = 0;
+        for (int number : nums) {    
+            result ^= number;
+        }
+        return result;  
+    }
+}
