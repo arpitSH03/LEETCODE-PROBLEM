ff208af85c22ee04e3dd68ccdf129cc4f9bfe83d
Arpit Sharma <arpitvgu.ac.in@gmail.com>
2026-09-23 10:40:04 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 13.99%] - LeetPush

--- 704-Binary-Search.java
+++ 704-Binary-Search.java
@@ -1,22 +1,20 @@
 class Solution {
-    public int search(int[] nums, int target) {
-         int left = 0;
-        int right = nums.length - 1;
-        int firstTrueIndex = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+       
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-            if (nums[mid] >= target) {
-                firstTrueIndex = mid;
-                right = mid - 1;
+            if (arr[mid] < target) {
+                low = mid + 1; 
+            } else if (arr[mid] > target) { 
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                return mid;
             }
         }
-        if (firstTrueIndex != -1 && nums[firstTrueIndex] == target) {
-            return firstTrueIndex;
-        }
-        return -1;
-        
+           
+        return -1; 
     }
 }
